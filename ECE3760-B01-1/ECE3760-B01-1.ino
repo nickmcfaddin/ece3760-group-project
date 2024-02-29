418263e013d3afea84171571908c183331026500
kameron <ronaldk1@myumanitoba.ca>
2024-02-28 22:42:17 -0600

    refactoring and stuffs

--- ECE3760-B01-1/ECE3760-B01-1.ino
+++ ECE3760-B01-1/ECE3760-B01-1.ino
@@ -1,20 +1,26 @@
-// === INLCUDE STATEMENTS =========================================================================
+// === COMPILATION "FLAGS" ==========================================================================================================================
+
+// #define SKIP_DEVICE
+#define DEBUG
+
+
+// === INLCUDE STATEMENTS ===========================================================================================================================
+
 #include "WiFi.h"
 #include "esp_now.h"
-#include <Adafruit_NeoPixel.h>
 
-// === DEFINE STATEMENTS ==========================================================================
+#ifndef SKIP_DEVICE
+#include <Adafruit_NeoPixel.h>
+#endif
+
+
+// === DEFINE STATEMENTS ============================================================================================================================
+
 #define BAUD_RATE 115200
 #define SYS_DELAY 100
 
-#define SKIP  0
-#define SWEEP 1
-
 #define LEFT  1
 #define RIGHT 2
-
-#define FALLING_EDGE 1
-#define RISING_EDGE  2
 
 #define PB_1_PIN 36
 #define PB_2_PIN 39
@@ -41,7 +47,7 @@
 #define RGB2_B_CHANNEL 5
 
 #define LED_STRIP_PIN  1
-#define LED_NUM_PIXELS 12
+#define LED_NUM_PIXELS 8
 
 #define PWM_FREQUENCY 5000  // 5000Hz frequency
 #define PWM_RESOLUTION   8  // 8 bits of resolution (0-255)
@@ -55,81 +61,102 @@
 #define PURPLE    0xFF00FF
 #define WHITE     0xFFFFFF
 
-#define LOWER_THRESHOLD      1200
-#define UPPER_THRESHOLD      2800
-#define CLEAR_THRESHOLD      900
-#define SWITCH_OFF_THRESHOLD 100
+#define LOWER_THRESHOLD 1200
+#define UPPER_THRESHOLD 2800
+#define CLEAR_THRESHOLD 900
 
 
 // === STRUCTS & ENUMS ============================================================================
 
+typedef enum state_enum {
+  IDLE,
+  CLEAR,
+  SWEEP_LIGHT,
+  SWEEP_HARD
+} state_t;
+
 typedef struct esp_now_packet_struct {
-  int leftCmd;   // 0: clear, 1: low, 2: mid, 3: high
-  int rightCmd;  // 0: clear, 1: low, 2: mid, 3: high
-} esp_now_packet;
+  int leftState;
+  int rightState;
+} esp_now_packet_t;
 
 
 // === GLOBAL VARIABLES ===========================================================================
 
-int deviceType = SKIP;  // Used to determine if the device should act like a [SKIP] or [SWEEP] device
+#ifdef SKIP_DEVICE
 
-int skipInputPins[] = {PB_1_PIN, PB_2_PIN, JOYSTICK_X_PIN, JOYSTICK_X_PIN, JOYSTICK_X_PIN};  //     [SKIP]
-int sweepInputPins[] = {PB_1_PIN, SW_1_PIN, SW_2_PIN};  //                                          [SWEEP]
+float ledBrightness = 0.25;
 
-int jsValues[] = {0, 0, 0};  // Joystick (JS) last EMA values {x, y, sw}                            [SKIP]
-bool pbStates[] = {false, false};  // Push Button (PB) logical value                                [SKIP/SWEEP]
-int swPosition = 0;  // Switch (SW) current position                                                [SWEEP]
+bool pbLeft =  false;  // value of 'left'  push button (PB) [digital]
+bool pbRight = false;  // value of 'right' push button (PB) [digital]
 
-int rgb1Pins[] =     {RGB1_R_PIN,     RGB1_G_PIN,     RGB1_B_PIN    };  // RGB LED 1 PWM pins       [SKIP]
-int rgb2Pins[] =     {RGB2_R_PIN,     RGB2_G_PIN,     RGB2_B_PIN    };  // RGB LED 2 PWM pins       [SKIP]
-int rgb1Channels[] = {RGB1_R_CHANNEL, RGB1_G_CHANNEL, RGB1_B_CHANNEL};  // RGB LED 1 PWM channels   [SKIP]
-int rgb2Channels[] = {RGB2_R_CHANNEL, RGB2_G_CHANNEL, RGB2_B_CHANNEL};  // RGB LED 2 PWM channels   [SKIP]
+int jsX =  0;          // value of 'x' joystick (JS) position [analog]
+int jsY =  0;          // value of 'y' joystick (JS) position [analog]
+int jsSW = 0;          // value of joystick (JS) 'switch'     [analog]
 
-int pbLeft =  0;  // Index for aliasing push button (PB) left                                       [SKIP]
-int pbRight = 1;  // Index for aliasing push button (PB) right                                      [SKIP]
-int jsX =  0;     // Index for aliasing joystick (JS) x position                                    [SKIP]
-int jsY =  1;     // Index for aliasing joystick (JS) y position                                    [SKIP]
-int jsSW = 2;     // Index for aliasing joystick (JS) switch (sw)                                   [SKIP]
-int* leftLED = rgb1Channels;   // Aliase for RGB LED left PWM channels                              [SKIP]
-int* rightLED = rgb2Channels;  // Aliase for RGB LED right PWM channels                             [SKIP]
+int rgb1Pins[] =     {RGB1_R_PIN,     RGB1_G_PIN,     RGB1_B_PIN    };  // RGB LED 1 PWM pins
+int rgb2Pins[] =     {RGB2_R_PIN,     RGB2_G_PIN,     RGB2_B_PIN    };  // RGB LED 2 PWM pins
+int rgb1Channels[] = {RGB1_R_CHANNEL, RGB1_G_CHANNEL, RGB1_B_CHANNEL};  // RGB LED 1 PWM channels
+int rgb2Channels[] = {RGB2_R_CHANNEL, RGB2_G_CHANNEL, RGB2_B_CHANNEL};  // RGB LED 2 PWM channels
 
-int pbPower = 0;  // Index for aliasing push button (PB) power                                      [SWEEP]
+int* leftLED = rgb1Channels;   // Aliase for RGB LED left PWM channels
+int* rightLED = rgb2Channels;  // Aliase for RGB LED right PWM channels
 
-Adafruit_NeoPixel pixels(LED_NUM_PIXELS, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);  // LED Control      [SWEEP]
+#else
 
-esp_now_packet packet;  // Instance of ESP-NOW packet to send to connected clients                  [SKIP/SWEEP]
-esp_now_peer_info_t peerInfo;  // Information about connected peers                                 [SKIP]
+bool pbPower =  false;  // value of 'power' push button (PB) [digital]
+int swPosition = 0;     // value of 'position' switch (SW) [LEFT/RIGHT]
 
-uint8_t hostAddress[] =   {0x40, 0x22, 0xD8, 0xEA, 0x76, 0x30};  // TODO: determine dynamically     [SWEEP]
-uint8_t clientAddress[] = {0x40, 0x22, 0xD8, 0xEE, 0x6D, 0xE0};  // TODO: determine dynamically     [SKIP]
+Adafruit_NeoPixel pixels(LED_NUM_PIXELS, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+
+#endif
+
+esp_now_packet_t packet;       // Instance of ESP-NOW packet struct for stored sent/recieved data
+esp_now_peer_info_t peerInfo;  // Information about connected peers
+
+uint8_t hostAddress[] =   {0x40, 0x22, 0xD8, 0xEA, 0x76, 0x30};  // TODO: determine dynamically
+uint8_t clientAddress[] = {0x40, 0x22, 0xD8, 0xEE, 0x6D, 0xE0};  // TODO: determine dynamically
 
 
 // === MAIN PROGRAM ===============================================================================
 
 void setup() {
+
+  #ifdef DEBUG
   // Configure Serial Communication
-  configureSerial();
+  Serial.begin(BAUD_RATE);
+  #endif
 
   // Configure Wifi
   WiFi.mode(WIFI_MODE_STA);
+  #ifdef DEBUG
   Serial.print("Mac Address: ");
   Serial.println(WiFi.macAddress());  // TODO: fix bug on SWEEP devices (MAC gets printed as non-sense)
+  #endif
 
-  if (deviceType == SKIP) {
+  // Configure ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    #ifdef DEBUG
+    Serial.println("Error initializing ESP-NOW");
+    #endif
+    return;
+  }
+
+#ifdef SKIP_DEVICE
   
     // Configure input GPIOs
-    configureGPIOs(skipInputPins, INPUT, 5);  // configure all input GPIOs
+    pinMode(PB_1_PIN, INPUT);
+    pinMode(PB_2_PIN, INPUT);
+    pinMode(JOYSTICK_X_PIN, INPUT);
+    pinMode(JOYSTICK_Y_PIN, INPUT);
+    pinMode(JOYSTICK_SW_PIN, INPUT);
 
     // Configure PWM channels
     configurePWM(rgb1Pins, rgb1Channels, 3);  // configure PWM for RGB LED 1
     configurePWM(rgb2Pins, rgb2Channels, 3);  // configure PWM for RGB LED 2
 
-    // Configure ESP-NOW
-    if (esp_now_init() != ESP_OK) {
-      Serial.println("Error initializing ESP-NOW");
-      return;
-    }
-    esp_now_register_send_cb(sent);
+    // Register callback function for 'on sent' event
+    esp_now_register_send_cb(onSent);
 
     // Register peer (TODO: setup mulitple peers)
     peerInfo.channel = 0;  
@@ -137,71 +164,73 @@ void setup() {
 
     // Register first peer (TODO: have 'pairing' process for dynamic connection)
     memcpy(peerInfo.peer_addr, clientAddress, 6);
-    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+      #ifdef DEBUG
       Serial.println("Failed to add peer");
+      #endif
       return;
     }
 
     delay(200);  // Extra delay to ensure ESP-NOW has enough time to configure
 
     // Initialize LED colors
-    setRGBA(rgb1Channels, BLUE, 0.25);
-    setRGBA(rgb2Channels, BLUE, 0.25);
+    setPwmRGBA(rgb1Channels, BLUE, ledBrightness);
+    setPwmRGBA(rgb2Channels, BLUE, ledBrightness);
 
-  }
-
-  if (deviceType == SWEEP) {
+#else
     
     // Configure input GPIOs
-    configureGPIOs(sweepInputPins, INPUT, 3);  // configure all input GPIOs
+    pinMode(PB_1_PIN, INPUT);
+    pinMode(SW_1_PIN, INPUT);
+    pinMode(SW_2_PIN, INPUT);
 
     // Configure NeoPixel
     pixels.begin();
 
-    // Configure ESP-NOW
-    if (esp_now_init() != ESP_OK) {
-      Serial.println("Error initializing ESP-NOW");
-      return;
-    }
-    esp_now_register_recv_cb(recieve);
+    // Register callback function for 'on recieve' event
+    esp_now_register_recv_cb(onRecieve);
 
     // Initialize LED colors
-    setStripRGBA(BLUE, 0.10);
-  }
+    setStripRGBA(BLUE, ledBrightness);
+
+#endif
+
 }
 
 void loop() {
 
-  if (deviceType == SKIP) {
-    // Update push button (PB) states
-    pbStates[pbLeft] = digitalRead(PB_1_PIN);
-    pbStates[pbRight] = digitalRead(PB_2_PIN);
-    // Update joystick (JS) values
-    updateJoystickValues();
-    // Perform Skip Logic
-    skipLogic();
-  }
+#ifdef SKIP_DEVICE
 
-  if (deviceType == SWEEP) {
-    // Update push button (PB) states
-    pbStates[pbPower] = digitalRead(PB_1_PIN);
-    // Update switch (SW) position
-    updateSwitchPosition();
-    // Perform Sweep Logic
+    // 1. Update push button (PB) values
+    pbLeft = digitalRead(PB_1_PIN);
+    pbRight = digitalRead(PB_2_PIN);
+    
+    // 2. Update joystick (JS) values
+    jsX = analogRead(JOYSTICK_X_PIN);
+    jsY = analogRead(JOYSTICK_Y_PIN);
+    jsSW = analogRead(JOYSTICK_SW_PIN);
+
+    // 3. Perform Skip Logic
+    skipLogic();
+
+#else
+
+    // 1. Update push button (PB) values
+    pbPower = digitalRead(PB_1_PIN);
+
+    // 2. Update switch (SW) value
+    swPosition = digitalRead(SW_1_PIN) ? LEFT : RIGHT;
+
+    // 3. Perform Sweep Logic
     sweepLogic();
-  }
+
+#endif
   
   delay(SYS_DELAY);  // System delay on every iteration
 }
 
 
-// === GPIO FUNCTIONS =============================================================================
-
-void configureGPIOs(int* pins, int direction, int count) {
-  for (int i = 0; i < count; i++) {
-    pinMode(pins[i], direction);
-  }
-}
+// === GPIO FUNCTIONS ===============================================================================================================================
 
 void configurePWM(int* pin, int* channel, int count) {
   for (int i = 0; i < count; i++) {
@@ -211,13 +240,14 @@ void configurePWM(int* pin, int* channel, int count) {
 }
 
 // Set the Red (R) Blue (B) Green (G) and Alpha (A) values for a PWM controlled RGB LED
-void setRGBA(int* channels, int rgb, float alpha) {
+void setPwmRGBA(int* channels, int rgb, float alpha) {
   ledcWrite(channels[0], (int)(((rgb >> 16) & 0xFF) * alpha));
   ledcWrite(channels[1], (int)(((rgb >> 8 ) & 0xFF) * alpha));
   ledcWrite(channels[2], (int)(((rgb >> 0 ) & 0xFF) * alpha));
 }
 
 void setStripRGBA(int rgb, float alpha) {
+#ifndef SKIP_DEVICE
   pixels.clear();
   for (int i = 0; i < LED_NUM_PIXELS; i++) {
     pixels.setPixelColor(i, pixels.Color(
@@ -227,165 +257,116 @@ void setStripRGBA(int rgb, float alpha) {
     ));
   }
   pixels.show();
-}
-
-void updateJoystickValues() {
-  jsValues[0] = analogRead(JOYSTICK_X_PIN);
-  jsValues[1] = analogRead(JOYSTICK_Y_PIN);
-  jsValues[2] = analogRead(JOYSTICK_SW_PIN);
-
-  serialPlot(jsValues[0], "x", false);
-  serialPlot(jsValues[1], "y", false);
-  serialPlot(jsValues[2], "sw", true);
-}
-
-void updateSwitchPosition() {
-  if (digitalRead(SW_1_PIN)) {
-    swPosition = 1;
-  } else if (digitalRead(SW_2_PIN)) {
-    swPosition = 2;
-  }
+#endif
 }
 
 
-// === SERIAL FUNCTIONS ===========================================================================
+// === LOGIC FUNCTIONS ==============================================================================================================================
 
-void configureSerial() {
-  Serial.begin(BAUD_RATE);
-}
-
-void serialPlot(int value, char* label, bool endLine) {
-  Serial.printf("%s:%d,", label, value);
-  if (endLine) Serial.print("\n");
-}
-
-
-// === AVERAGING FUNCTIONS ========================================================================
-
-// void updateEMA(int* currentValues, int* lastValues, int numValues, float k) {
-//   // Update exponential moving average (EMA)
-//   for (int i = 0; i < numValues; i++) {
-//     lastValues[i] = (int)((currentValues[i] * k) + (lastValues[i] * (1 - k)));
-//   }
-// }
-
-
-// === LOGIC FUNCTIONS ============================================================================
+#ifdef SKIP_DEVICE
 
 void skipLogic() {
-  if (jsValues[jsX] < LOWER_THRESHOLD - CLEAR_THRESHOLD) {
 
-    // Joystick LEFT: clear left LED
-    setRGBA(leftLED, BLUE, 0.25);
+  // 1. Check for LEFT joystick position
+  if (jsX < LOWER_THRESHOLD - CLEAR_THRESHOLD) {
+    setPwmRGBA(leftLED, BLUE, ledBrightness);       // set left LED 'blue'
+    packet.leftState = IDLE;                        // update left state
 
-    // Update client
-    packet.leftCmd = 0;
-
-  } else if (jsValues[jsX] > UPPER_THRESHOLD + CLEAR_THRESHOLD) {
-
-    // Joystick RIGHT: clear right LED
-    setRGBA(rightLED, BLUE, 0.25);
-
-    // Update client
-    packet.rightCmd = 0;
+  // 2. Check for RIGHT joystick position
+  } else if (jsX > UPPER_THRESHOLD + CLEAR_THRESHOLD) {
+    setPwmRGBA(rightLED, BLUE, ledBrightness);      // set left LED 'blue'
+    packet.rightState = IDLE;                       // update right state
 
   } else {
 
-    // Push Button LEFT
-    if (pbStates[pbLeft]) {
-      if (jsValues[1] < LOWER_THRESHOLD) {
-        // Joystick DOWN: left LED (red)
-        setRGBA(leftLED, RED, 0.8);
-        packet.leftCmd = 1;
-      } else if (jsValues[1] > UPPER_THRESHOLD) {
-        // Joystick UP: left LED (green)
-        setRGBA(leftLED, GREEN, 0.8);
-        packet.leftCmd = 3;
+    // 3. Check for LEFT button press
+    if (pbLeft) {
+      if (jsY < LOWER_THRESHOLD) {
+        setPwmRGBA(leftLED, RED, ledBrightness);     // set left LED 'red'
+        packet.leftState = SWEEP_LIGHT;              // update left state
+      } else if (jsY > UPPER_THRESHOLD) {
+        setPwmRGBA(leftLED, GREEN, ledBrightness);   // set left LED 'green'
+        packet.leftState = SWEEP_HARD;               // update left state
       } else {
-        // Joystick MID: left LED (yellow)
-        setRGBA(leftLED, YELLOW, 0.8);
-        packet.leftCmd = 2;
+        setPwmRGBA(leftLED, BLUE, ledBrightness);    // set left LED 'blue'
+        packet.leftState = CLEAR;                    // update left state
       }
     }
 
-    // Push Button RIGHT
-    if (pbStates[pbRight]) {
-      if (jsValues[1] < LOWER_THRESHOLD) {
-        // Joystick DOWN: right LED (red)
-        setRGBA(rightLED, RED, 0.8);
-        packet.rightCmd = 1;
-      } else if (jsValues[1] > UPPER_THRESHOLD) {
-        // Joystick UP: right LED (green)
-        setRGBA(rightLED, GREEN, 0.8);
-        packet.rightCmd = 3;
+    // 4. Check for RIGHT button press
+    if (pbRight) {
+      if (jsY < LOWER_THRESHOLD) {
+        setPwmRGBA(rightLED, RED, ledBrightness);    // set right LED 'red'
+        packet.rightState = SWEEP_LIGHT;             // update right state
+      } else if (jsY > UPPER_THRESHOLD) {
+        setPwmRGBA(rightLED, GREEN, ledBrightness);  // set right LED 'green'
+        packet.rightState = SWEEP_HARD;              // update right state
       } else {
-        // Joystick MID: left LED (yellow)
-        setRGBA(rightLED, YELLOW, 0.8);
-        packet.rightCmd = 2;
+        setPwmRGBA(rightLED, BLUE, ledBrightness);   // set right LED 'blue'
+        packet.rightState = CLEAR;                   // update right state
       }
     }
   }
 
-  transmit(); // update client
+  transmit();  // update client
 }
+
+#else
 
 void sweepLogic() {
-  // TODO: any logic required by the sweep during runtime (ex. timeout)
-}
 
-void sweepProcessCommand(int command) {
-  switch (command) {
-    case 0:
-      setStripRGBA(BLUE, 0.10);
+  // 1. Determine which 'side' to listen
+  int currentState = (swPosition == LEFT) ? packet.leftState : packet.rightState;
+
+  // 2. Determine which 'state' to set the LEDs
+  switch (currentState) {
+    case IDLE:
+      setStripRGBA(BLUE, ledBrightness);   // set LEDs 'blue'
       break;
-    case 1:
-      setStripRGBA(RED, 0.10);
+    case CLEAR:
+      setStripRGBA(BLUE, ledBrightness);   // set LEDs 'blue'
       break;
-    case 2:
-      setStripRGBA(YELLOW, 0.10);
+    case SWEEP_LIGHT:
+      setStripRGBA(RED, ledBrightness);    // set LEDs 'red'
       break;
-    case 3:
-      setStripRGBA(GREEN, 0.10);
+    case SWEEP_HARD:
+      setStripRGBA(GREEN, ledBrightness);  // set LEDs 'green'
       break;
   }
 }
+
+#endif
 
 
 // === ESP-NOW FUNCTIONS ==========================================================================
 
 void transmit() {
-  // Broadcast packet
-  esp_err_t result = esp_now_send(0, (uint8_t *) &packet, sizeof(esp_now_packet));
+  esp_err_t result = esp_now_send(0, (uint8_t *) &packet, sizeof(esp_now_packet_t));
+  #ifdef DEBUG
   Serial.printf("Transmit: %s\n", result == ESP_OK ? "SUCCESS" : "FAIL");
+  #endif
 }
 
-// Callback function when data is recieved
-void recieve(const uint8_t* macAddress, const uint8_t* data, int length) {
-  char macAddressString[18];  // string for holding the sender's MAC address
+void onRecieve(const uint8_t* macAddress, const uint8_t* data, int length) {
+  #ifdef DEBUG
+  char macAddressString[18];               // string for holding the sender's MAC address
   printMAC(macAddress, macAddressString);  // 'cast' MAC address to string
+  Serial.printf("Packet from '%s' recieved with length %d\n", macAddressString, length);
+  Serial.printf(" left: %d\n right: %d\n", (int)packet.leftState, (int)packet.rightState);
+  #endif
 
   memcpy(&packet, data, sizeof(packet));  // copy contents of packet into memory
-
-  Serial.printf("Packet from '%s' recieved with length %d\n", macAddressString, length);
-  Serial.printf(" left: %d\n right: %d\n", (int)packet.leftCmd, (int)packet.rightCmd);
-
-  // Process the latest skip command
-  if (swPosition == LEFT) {
-    sweepProcessCommand(packet.leftCmd);
-  } else if (swPosition == RIGHT) {
-    sweepProcessCommand(packet.rightCmd);
-  }
 }
 
-// Callback function when data is sent
-void sent(const uint8_t* macAddress, esp_now_send_status_t status) {
-  char macAddressString[18];  // string for holding the sender's MAC address
+void onSent(const uint8_t* macAddress, esp_now_send_status_t status) {
+  #ifdef DEBUG
+  char macAddressString[18];               // string for holding the sender's MAC address
   printMAC(macAddress, macAddressString);  // 'cast' MAC address to string
-  
   Serial.printf("Packet to '%s' send status: %s\n", macAddressString, status == ESP_NOW_SEND_SUCCESS ? "SUCCESS" : "FAIL");
+  #endif
 }
 
+// Copy MAC address to string
 void printMAC(const uint8_t* macAddress, char* macString) {
-  // Copy MAC address to string
   snprintf(macString, sizeof(macString), "%02x:%02x:%02x:%02x:%02x:%02x", macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]);
 }
